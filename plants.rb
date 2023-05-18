c1fa218367b1e4bcd8f24ee8552b762cf2a7e65d
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-17 19:34:48 -0500

    Add a new conditional

--- plants.rb
+++ plants.rb
@@ -1,8 +1,10 @@
-git_sunshine = true
-ounces_of_water = 11
+git_sunshine = false
+ounces_of_water = 9
 
 if git_sunshine == true && ounces_of_water >= 10
     puts "Plant is thriving"
-else 
+elsif git_sunshine == true || ounces_of_water >= 10
+    puts "Plant needs some help"
+else
     puts "Plant is dead"
 end
